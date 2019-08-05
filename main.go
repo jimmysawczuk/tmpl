9ddd84a55893f37b6c0880165cd7a9717356850d
Jimmy Sawczuk <github@jimmysawczuk.com>
2019-08-04 21:28:07 -0400

    WIP

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/bmatcuk/doublestar"
 	"github.com/joho/godotenv"
@@ -15,7 +16,7 @@ import (
 
 var config = struct {
 	watch   bool
-	output  string
+	out     string
 	fmt     string
 	envFile string
 
@@ -26,7 +27,7 @@ var config = struct {
 
 func init() {
 	flag.BoolVar(&config.watch, "w", false, "run continuously, watching for changes and rewriting file as needed")
-	flag.StringVar(&config.output, "o", "", "output destination ('' means stdout)")
+	flag.StringVar(&config.out, "out", "", "output destination ('' means stdout)")
 	flag.StringVar(&config.fmt, "fmt", "", "output format ('', 'html' or 'json')")
 	flag.StringVar(&config.envFile, "env-file", "", "pipe .env file before executing template")
 	flag.BoolVar(&config.timestampAssets, "timestamp-assets", true, "set to false to not automatically timestamp production assets")
@@ -47,49 +48,79 @@ func main() {
 	}
 
 	var ins []io.Reader
+	var prefixes []string
 	if flag.Arg(0) == "--" {
 		ins = append(ins, os.Stdin)
 	} else {
 		matches, err := doublestar.Glob(flag.Arg(0))
 		if err != nil {
-			fatalErr(errors.Wrapf(err, "glob: %s"))
+			fatalErr(errors.Wrapf(err, "glob: %s", flag.Arg(0)))
 		}
 
 		for _, m := range matches {
+			fi, err := os.Stat(m)
+			if err != nil {
+				log.Println(errors.Wrapf(err, "stat file %s", m))
+				continue
+			}
+
+			if fi.IsDir() {
+				continue
+			}
+
 			fp, err := os.Open(m)
 			if err != nil {
 				log.Println(errors.Wrapf(err, "open file %s", m))
 				continue
 			}
+
 			ins = append(ins, fp)
+			prefixes = append(prefixes, fp.Name())
 		}
 	}
+
+	prefix := longestCommonPrefix(prefixes)
+	log.Println("prefix", prefix)
 
 	var outs []io.Writer
 	for _, v := range ins {
 		var out io.Writer = os.Stdout
 
-		if config.output != "" {
+		if config.out != "" {
 			if len(ins) > 1 {
-				fi, err := os.Stat(config.output)
+				fi, err := os.Stat(config.out)
 				if err != nil {
-					fatalErr(errors.Wrapf(err, "stat output %s", config.output))
+					if os.IsNotExist(err) {
+						if err := os.MkdirAll(config.out, 0755); err != nil {
+							fatalErr(errors.Wrapf(err, "create directory %s", config.out))
+						}
+
+						fi, err = os.Stat(config.out)
+						if err != nil {
+							fatalErr(errors.Wrapf(err, "stat output %s", config.out))
+						}
+					} else {
+						fatalErr(errors.Wrapf(err, "stat output %s", config.out))
+					}
 				}
 
 				if !fi.IsDir() {
 					fatalErr(errors.New("directory is required for more than one input"))
 				}
 
-				outpath := filepath.Clean(config.output) + "/" + v.(*os.File).Name()
+				name := strings.Replace(v.(*os.File).Name(), prefix, "", 1)
+
+				outpath := filepath.Clean(config.out + "/" + name)
 
 				out, err = os.OpenFile(outpath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+				if err != nil {
+					fatalErr(err)
+				}
 			}
 		}
 
 		outs = append(outs, out)
 	}
-
-	log.Println(ins, outs)
 
 	for i := range ins {
 		switch config.fmt {
@@ -159,4 +190,25 @@ func openOutputFile(path string) (io.Writer, error) {
 func fatalErr(err error) {
 	fmt.Fprintf(os.Stderr, "%s\n", err)
 	os.Exit(2)
+}
+
+func longestCommonPrefix(ins []string) string {
+	prefix := ""
+	j := 0
+	for {
+		for i := 1; i < len(ins); i++ {
+			if j > len(ins[0]) || j > len(ins[i]) {
+				return prefix
+			}
+
+			f0 := ins[0]
+			fi := ins[i]
+			if f0[j] != fi[j] {
+				return prefix
+			}
+		}
+
+		prefix = prefix + string(ins[0][j])
+		j++
+	}
 }
