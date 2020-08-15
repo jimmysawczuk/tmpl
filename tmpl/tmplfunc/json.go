6f6a5da4bc674dce63be66909c30d86a36a72246
Jimmy Sawczuk <me@jimmysawczuk.com>
2020-08-14 20:49:42 -0400

    Enhanced error handling

--- tmpl/tmplfunc/json.go
+++ tmpl/tmplfunc/json.go
@@ -3,13 +3,18 @@ package tmplfunc
 import (
 	"encoding/json"
 	"io/ioutil"
+	"path/filepath"
+
+	"github.com/pkg/errors"
 )
 
 // GetJSON reads data from the provided (local) path, and attempts to unmarshal it.
 func GetJSON(path string) (interface{}, error) {
+	abspath, _ := filepath.Abs(path)
+
 	by, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrapf(err, "read file (path: %s, abs path: %s)", path, abspath)
 	}
 
 	var target interface{}
